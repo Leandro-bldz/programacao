f1298a01c224ce971401844cfab83c95b3ddeb44
04048275 <lbaldez94@gmail.com>
2019-09-10 10:15:06 -0300

    Questao 2

---
+++ src/sample/Questao02.java
@@ -0,0 +1,15 @@
+package sample;
+
+import java.util.Scanner;
+
+public class Questao02 {
+    public static void main(String[] args) {
+    double numero;
+    double dobro;
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("Informe o numero: ");
+    numero = scanner.nextDouble();
+    dobro = numero * 2;
+        System.out.println("O dobro do numero é: " + dobro);
+    }
+}
